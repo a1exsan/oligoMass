69a28faac7e9d1b0c2eb127b67a7e97ac78aac82
alex <gominsan@gmail.com>
2022-04-12 14:16:26 +0700

    built version 6.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='oligoMass',
-    version='0.0.5',
+    version='0.0.6',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     install_requires=['molmass', 'pandas']
